3c5c850a53e4989338627b480efef2fb84b5899e
Nima <74507096+Nimausfi@users.noreply.github.com>
2020-12-28 09:52:00 -0800

    Update aluarith.vhd

--- srcs/aluarith.vhd
+++ srcs/aluarith.vhd
@@ -2,7 +2,6 @@ library ieee ;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
 
----------------------------------------------
 
 entity aluarith is
 
@@ -17,7 +16,6 @@ port(
 ); 
 end aluarith;
 
-----------------------------------------------
 
 architecture behv of aluarith is
    
